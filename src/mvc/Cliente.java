341639389196d0bf37821b2582dffbab5dbdcf9f
Fernando <a21110132@ceti.mx>
2022-06-11 15:13:55 -0500

    Clases cliente y compra

--- src/mvc/Cliente.java
+++ src/mvc/Cliente.java
@@ -1,14 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Equipo4
 package mvc;
 
-/**
- *
- * @author srico
- */
 public class Cliente {
+    private int id;
+    private String nombre;
+    private int tipo;
+    private int compras;
+    private boolean incidentes;
+    
+    //CONSTRUCTOR
+
+    public Cliente(int id, String nombre, int tipo, int compras, boolean incidentes) {
+        this.id = id;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.compras = compras;
+        this.incidentes = incidentes;
+    }
+    
+    //SETTER N GETTER 
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+ 
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getCompras() {
+        return compras;
+    }
+
+    public void setCompras(int compras) {
+        this.compras = compras;
+    }
+
+    public boolean isIncidentes() {
+        return incidentes;
+    }
+
+    public void setIncidentes(boolean incidentes) {
+        this.incidentes = incidentes;
+    }
     
 }
--- src/mvc/Compra.java
+++ src/mvc/Compra.java
@@ -1,14 +1,62 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//EQUIPO4
 package mvc;
 
-/**
- *
- * @author srico
- */
 public class Compra {
+    private int id;
+    private String titulo;
+    private int tipo_compra;
+    private float monto;
+    private int tipo_pago;
+     
+    //CONSTRUCTOR 
+
+    public Compra(int id, String titulo, int tipo_compra, float monto, int tipo_pago) {
+        this.id = id;
+        this.titulo = titulo;
+        this.tipo_compra = tipo_compra;
+        this.monto = monto;
+        this.tipo_pago = tipo_pago;
+    }
     
+    //SETTER N GETTER 
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getTipo_compra() {
+        return tipo_compra;
+    }
+
+    public void setTipo_compra(int tipo_compra) {
+        this.tipo_compra = tipo_compra;
+    }
+
+    public float getMonto() {
+        return monto;
+    }
+
+    public void setMonto(float monto) {
+        this.monto = monto;
+    }
+
+    public int getTipo_pago() {
+        return tipo_pago;
+    }
+
+    public void setTipo_pago(int tipo_pago) {
+        this.tipo_pago = tipo_pago;
+    }
 }
