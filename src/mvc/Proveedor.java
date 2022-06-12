46bbcab2342b8137a8ea030a4402ec1591605577
samricodev <sricoe6.8@dgb.email>
2022-06-11 21:14:12 -0500

    "Clases bases terminadas"

--- src/mvc/Proveedor.java
+++ src/mvc/Proveedor.java
@@ -2,5 +2,57 @@
 package mvc;
 
 public class Proveedor {
-    
+    private int id;
+    private String nombre;
+    private String categoria;
+    private String productos;
+    private float cobro;
+
+    Proveedor(int id, String nombre, String categoria, String productos, float cobro) {
+        this.id = id;
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.productos = productos;
+        this.cobro = cobro;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getCategoria(){
+        return categoria;
+    }
+
+    public void setCategoria(String categoria){
+        this.categoria = categoria;
+    }
+
+    public String getProductos(){
+        return productos;
+    }
+
+    public void setProductos(String productos){
+        this.productos = productos;
+    }
+
+    public float getCobro(){
+        return cobro;
+    }
+
+    public void setCobro(float cobro){
+        this.cobro = cobro;
+    }
 }
