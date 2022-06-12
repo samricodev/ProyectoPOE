05f1e7276910b73db8917b3661893184611022b2
samricodev <sricoe6.8@dgb.email>
2022-06-11 22:50:37 -0500

    "Menu principal editado"

--- src/mvc/frmMenuPrincipal.java
+++ src/mvc/frmMenuPrincipal.java
@@ -1,19 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mvc;
 
-/**
- *
- * @author srico
- */
 public class frmMenuPrincipal extends javax.swing.JFrame {
 
-    /**
-     * Creates new form frmMenuPrincipal
-     */
     public frmMenuPrincipal() {
         initComponents();
     }
